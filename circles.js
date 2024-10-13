57fc95ff640450fb1fa06130d71f9162547ff5cf
William <71906624+Williamlowr@users.noreply.github.com>
2024-10-12 22:54:27 -0500

    finish

--- circles.js
+++ circles.js
@@ -1,78 +1,69 @@
-function width(x, y)
-{
+function width(x, y) {
   var points = [];
   points.push([x, y]);
-  for (var d=1; d<=1; d++)
-  {
-    points.push([x, y-d]);
-    points.push([x, y+d]);
+  // Add thickness of 12
+  for (var d = 1; d <= 12; d++) {
+    points.push([x - d / 4, y - d]);
+    points.push([x + d / 4, y + d]);
   }
-  return points
-}
-
-function plot(x, y)
-{
-  var points = [];
-  points = points.concat(width(x, y));
-  points = points.concat(width(x, -1*y));
-  points = points.concat(width(-1*x, y));
-  points = points.concat(width(-1*x, -1*y));
-  points = points.concat(width(y, x));
-  points = points.concat(width(-1*y, x));
-  points = points.concat(width(y, -1*x));
-  points = points.concat(width(-1*y, -1*x));
   return points;
 }
 
+function plot(x, y, centerX, centerY) {
+  var points = [];
+  // Map symmetry
+  points = points.concat(width(x, y));
+  points = points.concat(width(x, -1 * y));
+  points = points.concat(width(-1 * x, y));
+  points = points.concat(width(-1 * x, -1 * y));
+  points = points.concat(width(y, x));
+  points = points.concat(width(-1 * y, x));
+  points = points.concat(width(y, -1 * x));
+  points = points.concat(width(-1 * y, -1 * x));
 
-function drawCircle(r)
-{
+  for (var i = 0; i < points.length; i++) {
+    points[i][0] = points[i][0] + centerX;
+    points[i][1] = points[i][1] + centerY;
+  }
 
-// Midpoint Circle Algorithm plotting top sector of circle 
+  return points;
+}
+
+function drawCircle(r, centerX, centerY) {
+  // Midpoint Circle Algorithm plotting top sector of circle
   var vertices = [];
   var points = [];
 
   var x0 = 0;
   var y0 = r;
-  points = plot (x0, y0);
-  for (var i=0; i<points.length; i++)
-  {
+  points = plot(x0, y0, centerX, centerY);
+  for (var i = 0; i < points.length; i++) {
     vertices.push(points[i]);
   }
 
   var p0 = 1 - r;
-
   var xk = x0;
   var yk = y0;
   var pk = p0;
 
-  while (xk < yk)
-  {
-    if (pk < 0)
-    {
+  while (xk < yk) {
+    if (pk < 0) {
       xk = xk + 1;
-      points = plot (xk, yk);
-      for (var i=0; i<points.length; i++)
-      {
+      points = plot(xk, yk, centerX, centerY);
+      for (var i = 0; i < points.length; i++) {
         vertices.push(points[i]);
       }
-      pk = pk + (2 * xk) + 1;
-    }
-    else
-    {
+      pk = pk + 2 * xk + 1;
+    } else {
       xk = xk + 1;
       yk = yk - 1;
-      points = plot (xk, yk);
-      for (var i=0; i<points.length; i++)
-      {
+      points = plot(xk, yk, centerX, centerY);
+      for (var i = 0; i < points.length; i++) {
         vertices.push(points[i]);
       }
-      pk = pk + (2 * xk) + 1 - (2 * yk);
+      pk = pk + 2 * xk + 1 - 2 * yk;
     }
   }
 
   return vertices;
 }
-
-
-
--- lines.js
+++ lines.js
@@ -1,46 +1,63 @@
 function lineCheck(x0, y0, xend, yend) {
   var points = [];
-  var dx = (xend - x0);
-  var dy = (yend - y0);
-  var slope = dx === 0 ? Infinity : dy / dx;
+  var dx = xend - x0;
+  var dy = yend - y0;
+  // Customizable thickness
+  var thickness = 10;
 
-  console.log(slope);
-  console.log("Starting linecheck: ", x0, y0, xend, yend);
-  if (slope > 0 && slope <= 1) {
-    console.log("Reached 1");
-    points = drawLinePos(x0, y0, xend, yend, dx, dy);
-  } else if (slope > 1) {
-    console.log("2");
-    points = drawLinePos(y0, x0, yend, xend, dy, dx).map((p) => [p[1], p[0]]);
-  } else if (slope >= -1 && slope < 0) {
-    console.log("3");
-    points = drawLineNeg(x0, y0, xend, yend, dx, dy);
-  } else if (slope < -1) {
-    console.log("4");
-    points = drawLineSteepNeg(x0, y0, xend, yend, dy, dx).map((p) => [p[1], p[0]]);
-  } else if (dx === 0) {
-    console.log("5");
-    points = drawLineVertical(x0, y0, yend);
-  } else if (dy === 0) {
-    console.log("6");
-    points = drawLineHorizontal(x0, y0, xend);
+  // Check vertical
+  if (dx === 0) {
+    points = drawLineVertical(x0, y0, yend, thickness);
+    return points;
   }
-  
+
+  // Check horizontal
+  if (dy === 0) {
+    points = drawLineHorizontal(x0, y0, xend, thickness);
+    return points;
+  }
+
+  // Calculate slope
+  var slope = dy / dx;
+
+  // Slope cases
+  if (slope > 0 && slope <= 1) {
+    console.log("Pos " + slope);
+    points = drawLinePos(x0, y0, xend, yend, dx, dy, thickness);
+
+  } else if (slope > 1) {
+    console.log("StPos " + slope);
+    points = drawLineSteepPos(y0, x0, yend, xend, dy, dx, thickness).map(
+      (p) => [p[1], p[0]]
+    );
+
+  } else if (slope >= -1 && slope < 0) {
+    console.log("Neg " + slope);
+    points = drawLineNeg(x0, y0, xend, yend, dx, dy, thickness);
+
+  } else if (slope < -1) {
+    console.log("StNeg " + slope);
+    points = drawLineSteepNeg(y0, x0, yend, xend, dx, dy, thickness).map(
+      (p) => [p[1], p[0]]
+    );
+  }
+
   return points;
 }
 
-function drawLinePos(x0, y0, xend, yend, dx, dy) {
+function drawLinePos(x0, y0, xend, yend, dx, dy, thickness) {
   var points = [];
   var twodx = 2 * dx;
   var twody = 2 * dy;
   var twodydx = twody - twodx;
   var pk = twody - dx;
   var yk = y0;
-  
-  
 
   for (let xk = x0; xk <= xend; xk++) {
-    points.push([xk, yk]);
+    // Iterate through points, add vertical thickness
+    for (let width = -thickness; width <= thickness; width++) {
+      points.push([xk, yk + width]);
+    }
     if (pk < 0) {
       pk = pk + twody;
     } else {
@@ -51,72 +68,105 @@ function drawLinePos(x0, y0, xend, yend, dx, dy) {
   return points;
 }
 
-function drawLineNeg(x0, y0, xend, yend, dx, dy) {
+function drawLineSteepPos(y0, x0, yend, xend, dx, dy, thickness) {
   var points = [];
   var twodx = 2 * dx;
-  var yk = y0;
-  var absDY = Math.abs(dy);
-  var twody = 2 * absDY;
-  var twodydx = twody - twodx;
-  var pk = twody - dx;
-  console.log("Draw neg");
+  var twody = 2 * dy;
+  var twodxdy = twody - twodx;
+  var pk = twodx - dy;
+  var xk = x0;
 
-  console.log("x0")
-  console.log(x0);
-  console.log("xend")
-  console.log(xend);
+  if (x0 > xend) {
+    [x0, xend] = [xend, x0];
+    [y0, yend] = [yend, y0];
+    dx = xend - x0;
+    dy = yend - y0;
+  }
 
-  for (let xk = x0; xk <= xend; xk++) {
-    points.push([xk, yk]);
-    console.log(pk);
+  for (let yk = y0; yk <= yend; yk++) {
+    for (let width = -thickness; width <= thickness; width++) {
+      points.push([yk, xk + width]);
+    }
     if (pk < 0) {
-      pk = twodydx + pk;
+      pk = twodx + pk;
     } else {
-      yk--;
-      pk = pk + (2 * (absDY - dx));
+      xk++;
+      pk = pk + twodxdy;
     }
   }
   return points;
 }
 
-function drawLineSteepNeg(x0, y0, xend, yend, dx, dy) {
+function drawLineNeg(x0, y0, xend, yend, dx, dy, thickness) {
   var points = [];
-  var twody = 2 * dy;
-  var xk = x0;
-  var absDX = dx //Math.abs(dx);
   var twodx = 2 * dx;
-  var twodxdy = twodx - twody;
-  var pk = twodx - dy;
-  console.log("Draw neg");
+  var yk = y0;
+  var dy = -dy;
+  var twody = 2 * dy;
+  var twodydx = twody - twodx;
+  var pk = twody - dx;
 
-  console.log("starting point: ",  x0, y0);
-  console.log("ending point: ", xend, yend);
-
-  for (let yk = y0; yk >= yend; yk--) {
-    points.push([yk, xk]);
-    //console.log("Point: ", xk, yk);
-    //if (pk < 0) {
-    //  pk = twodxdy + pk;
-    //} else {
-      xk++;
-    //  pk = pk + (2 * (absDX - dy));
-    //}
+  for (let xk = x0; xk <= xend; xk++) {
+    for (let width = -thickness; width <= thickness; width++) {
+      points.push([xk, yk + width]);
+    }
+    if (pk < 0) {
+      pk = pk + twody;
+    } else {
+      yk--;
+      pk = pk + twodydx;
+    }
   }
   return points;
 }
 
-drawLineVertical = (x0, y0, xend, yend) => {
+function drawLineSteepNeg(y0, x0, yend, xend, dx, dy, thickness) {
+  var points = [];
+
+  if (y0 < yend) {
+    [y0, yend] = [yend, y0];
+    [x0, xend] = [xend, x0];
+    dx = xend - x0;
+    dy = yend - y0;
+  }
+
+  var xk = x0;
+  dy = -dy;
+  var twody = 2 * dy;
+  var twodx = 2 * dx;
+  var twodxdy = twodx - twody;
+  var pk = twodx - dy;
+
+  for (let yk = y0; yk >= yend; yk--) {
+    for (let width = -thickness; width <= thickness; width++) {
+      points.push([yk, xk + width]);
+    }
+    if (pk < 0) {
+      pk = twodx + pk;
+    } else {
+      xk++;
+      pk = pk + twodxdy;
+    }
+  }
+  return points;
+}
+
+drawLineVertical = (x0, y0, yend, thickness) => {
   let points = [];
-  for (let y = Math.min(y0, yend); (y <= Math.max(y0, yend)); y++) {
-    points.push([x0, y]);
+  for (let y = Math.min(y0, yend); y <= Math.max(y0, yend); y++) {
+    for (let width = -thickness; width <= thickness; width++) {
+      points.push([x0 - width, y]);
+    }
   }
   return points;
 };
 
-drawLineHorizontal = (x0, y0, xend) => {
+drawLineHorizontal = (x0, y0, xend, thickness) => {
   let points = [];
   for (let x = Math.min(x0, xend); x <= Math.max(x0, xend); x++) {
-    points.push([x, y0]);
+    for (let width = -thickness; width <= thickness; width++) {
+      points.push([x, y0 - width]);
+    }
   }
   return points;
 };
